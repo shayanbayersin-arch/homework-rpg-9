ae71ca6f7b614c90fb774053144afd4980d8f1e7
shayanbayersin-arch <shayanbayersin@gmail.com>
2026-05-05 22:11:26 +0500

    feat: implement Armor class with visitor double-dispatch support

--- src/com/narxoz/rpg/artifact/Armor.java
+++ src/com/narxoz/rpg/artifact/Armor.java
@@ -1,23 +1,14 @@
 package com.narxoz.rpg.artifact;
-
-/**
- * A protective suit, plate, or enchanted shield.
- */
 public class Armor extends Artifact {
-
     private final int defenseBonus;
-
     public Armor(String name, int value, int weight, int defenseBonus) {
         super(name, value, weight);
         this.defenseBonus = defenseBonus;
     }
-
-    public int getDefenseBonus() {
-        return defenseBonus;
-    }
+    public int getDefenseBonus() { return defenseBonus; }
 
     @Override
     public void accept(ArtifactVisitor visitor) {
-        // TODO: call visitor.visit(this) for double dispatch.
+        visitor.visit(this);
     }
 }
