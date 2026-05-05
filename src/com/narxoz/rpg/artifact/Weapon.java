38c6a07313c0d89845c874eec8b858c3b9f2477f
shayanbayersin-arch <shayanbayersin@gmail.com>
2026-05-05 22:10:28 +0500

    feat: implement Weapon class with visitor double-dispatch support

--- src/com/narxoz/rpg/artifact/Weapon.java
+++ src/com/narxoz/rpg/artifact/Weapon.java
@@ -1,23 +1,15 @@
 package com.narxoz.rpg.artifact;
 
-/**
- * A melee or ranged weapon found in the vault.
- */
 public class Weapon extends Artifact {
-
     private final int attackBonus;
-
     public Weapon(String name, int value, int weight, int attackBonus) {
         super(name, value, weight);
         this.attackBonus = attackBonus;
     }
-
-    public int getAttackBonus() {
-        return attackBonus;
-    }
+    public int getAttackBonus() { return attackBonus; }
 
     @Override
     public void accept(ArtifactVisitor visitor) {
-        // TODO: call visitor.visit(this) for double dispatch.
+        visitor.visit(this);
     }
 }
