ebc769a2a2a52939366e39d5e8fdabc8226ff918
shayanbayersin-arch <shayanbayersin@gmail.com>
2026-05-05 22:12:48 +0500

    feat: implement Potion, Ring, and Scroll classes with visitor support

--- src/com/narxoz/rpg/artifact/Potion.java
+++ src/com/narxoz/rpg/artifact/Potion.java
@@ -1,23 +1,8 @@
 package com.narxoz.rpg.artifact;
 
-/**
- * A restorative or alchemical potion stored in the vault.
- */
 public class Potion extends Artifact {
-
     private final int healing;
-
-    public Potion(String name, int value, int weight, int healing) {
-        super(name, value, weight);
-        this.healing = healing;
-    }
-
-    public int getHealing() {
-        return healing;
-    }
-
-    @Override
-    public void accept(ArtifactVisitor visitor) {
-        // TODO: call visitor.visit(this) for double dispatch.
-    }
+    public Potion(String name, int value, int weight, int healing) { super(name, value, weight); this.healing = healing; }
+    public int getHealing() { return healing; }
+    @Override public void accept(ArtifactVisitor visitor) { visitor.visit(this); }
 }
--- src/com/narxoz/rpg/artifact/Ring.java
+++ src/com/narxoz/rpg/artifact/Ring.java
@@ -1,23 +1,8 @@
 package com.narxoz.rpg.artifact;
 
-/**
- * A ring with a small enchantment or mystical bonus.
- */
 public class Ring extends Artifact {
-
     private final int magicBonus;
-
-    public Ring(String name, int value, int weight, int magicBonus) {
-        super(name, value, weight);
-        this.magicBonus = magicBonus;
-    }
-
-    public int getMagicBonus() {
-        return magicBonus;
-    }
-
-    @Override
-    public void accept(ArtifactVisitor visitor) {
-        // TODO: call visitor.visit(this) for double dispatch.
-    }
+    public Ring(String name, int value, int weight, int magicBonus) { super(name, value, weight); this.magicBonus = magicBonus; }
+    public int getMagicBonus() { return magicBonus; }
+    @Override public void accept(ArtifactVisitor visitor) { visitor.visit(this); }
 }
